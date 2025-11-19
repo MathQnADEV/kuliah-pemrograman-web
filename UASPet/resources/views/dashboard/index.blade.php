f83ebb23206dd949242fa98cebfbd34fc429039a
Shaquille Rashaun Sahl Tamrin <mathqna.project@gmail.com>
2025-11-19 19:42:30 +0800

    template awal

---
+++ UASPet/resources/views/dashboard/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div>
+        <h1 class="text-3xl font-bold text-center">Dashboard</h1>
+    </div>
+@endsection
---
+++ UASPet/resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Welcome')
+
+@section('content')
+    <h1 class="text-3xl font-bold text-center">Welcome to Pet</h1>
+@endsection
